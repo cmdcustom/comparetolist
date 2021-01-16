7fee9a8f9286e732d82755eea958cf66f79c0e14
cmdcustom <76268123+cmdcustom@users.noreply.github.com>
2021-01-15 21:26:01 -0700

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,6 +1,2 @@
 @echo off
-::Works just like for word in python
-::IK you can just use find "wordheredontremovequotes" filenamehere but whatever
-set filetoread=YourFileHere
-set wordtosearch=WordToSearchForHere
-type %filetoread% && FINDSTR %wordtosearch% && cls && goto :found || echo.Did not find %wordtosearch% on any line in %filetoread% >> log.txt
+type %1 && FINDSTR %2 && cls && goto :found || echo.Did not find %2 on any line in %1 >> log.txt
